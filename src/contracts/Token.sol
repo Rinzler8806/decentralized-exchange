d7e7f05acb40f093a234fac6356faa49fc62bcd0
Rinzler8806 <shane.rogers916@gmail.com>
2021-12-26 23:05:28 -0800

    wrote tests to track name, symbol, decimals and total supply. Tests passed.

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-    string public name = "My Name";
+    string public name = "Rinzler Token";
+    string public symbol = "RZLR";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
